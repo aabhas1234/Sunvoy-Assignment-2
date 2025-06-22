92c8d1edd2f9744420dffb27df0c422ce807edf3
aabhas1234 <136249872+aabhas1234@users.noreply.github.com>
2025-06-22 21:10:23 +0530

    Initial commit

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,5 +1,6 @@
 import type { CollectionConfig } from 'payload'
-
+import { LexicalEditor } from '@payloadcms/richtext-lexical/lexical'
+import { defaultEditorFeatures, lexicalEditor } from '@payloadcms/richtext-lexical'
 export const Users: CollectionConfig = {
   slug: 'users',
   admin: {
@@ -7,7 +8,18 @@ export const Users: CollectionConfig = {
   },
   auth: true,
   fields: [
-    // Email added by default
-    // Add more fields as needed
+    {
+      name:'Phone-no',
+      type:'number'
+    },
+    {
+      name:'Content',
+      type:'richText',
+      editor :lexicalEditor({admin: { placeholder: 'Type your content here ...'  },features: ({ defaultFeatures, rootFeatures }) => {
+        console.log(defaultFeatures);
+        return [...defaultFeatures, ...rootFeatures];
+      }})
+    }
   ],
+ 
 }
